3e7f52a9325f0b151aa7c6ba9e2bd28dd63fab07
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-08-23 19:57:51 +0300

    Fix footer value

--- src/pages/layout/footer/Footer.jsx
+++ src/pages/layout/footer/Footer.jsx
@@ -8,13 +8,12 @@ import { ROUTES } from '../../../routes/routesModel';
 import { useCurrentUser } from '../../../providers/UserProvider';
 
 export default function Footer() {
-  const [value, setValue] = useState(0);
   const navigate = useNavigate();
   const { user } = useCurrentUser();
   return (
     <Paper elevation={3} component='footer' sx={{ position: 'sticky', bottom: 0, left: 0, right: 0 }} width='100%'>
       <Divider />
-      <BottomNavigation showLabels value={value} onChange={(event, newValue) => { setValue(newValue) }}>
+      <BottomNavigation showLabels>
         <BottomNavigationAction label='About' icon={<InfoIcon />} onClick={() => navigate(ROUTES.ABOUT)} />
         {user ? (<BottomNavigationAction label='Favourites' icon={<FavoriteIcon />} onClick={() => navigate(ROUTES.FAV_CARDS)} />) : null}
         {user && user.isBusiness ? (<BottomNavigationAction label='My Cards' icon={<RecentActorsIcon />} onClick={() => navigate(ROUTES.MY_CARDS)} />) : null}
