86838856016ae34b8f004c79349c2f018efe160a
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-08-24 23:56:27 +0300

    Implement delete dialog

--- src/components/cards/CardActionBar.jsx
+++ src/components/cards/CardActionBar.jsx
@@ -1,8 +1,12 @@
-import { Box, CardActions, IconButton } from '@mui/material'
+import { Box, Button, CardActions, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, Slide } from '@mui/material'
 import { Delete as DeleteIcon, Edit as EditIcon, Phone as PhoneIcon, Favorite as FavoriteIcon } from '@mui/icons-material';
-import React, { useState } from 'react'
+import React, { forwardRef, useState } from 'react'
 import { useCurrentUser } from '../../providers/UserProvider';
 import { pink } from '@mui/material/colors';
+
+const Transition = forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export default function CardActionBar({ handleDel, handleLike, handleEdit, likes, cardId, userId }) {
     const { user } = useCurrentUser();
@@ -14,14 +18,29 @@ export default function CardActionBar({ handleDel, handleLike, handleEdit, likes
     const handleLikeCard = async () => {
         await handleLike(cardId);
         setIsLiked((prev) => !prev);
-    }
+    };
+
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleDeleteCard = () => {
+        handleClose();
+        handleDel();
+    };
 
     return (
         <>
             <CardActions sx={{ justifyContent: 'space-between' }}>
                 <Box>
                     {/* Detect if user is admin or owner if card - display delete icon */}
-                    {(user && user.isAdmin) || (user && user._id == userId) ? (<IconButton aria-label='delete' onClick={handleDel}>
+                    {(user && user.isAdmin) || (user && user._id == userId) ? (<IconButton aria-label='delete' onClick={handleClickOpen}>
                         <DeleteIcon />
                     </IconButton>) : null}
                     {/* Detect if user is admin or owner if card - display edit icon */}
@@ -38,6 +57,24 @@ export default function CardActionBar({ handleDel, handleLike, handleEdit, likes
                     </IconButton> : null}
                 </Box>
             </CardActions>
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle>{"Delete this business card?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        Are you sure you want to delete this business card? This action cannot be undone, and all information associated with this card will be permanently removed.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleDeleteCard} color='error'>Delete Business Card</Button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
