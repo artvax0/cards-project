2838612f11c8f0b2897af9521da9c62388d39e40
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-09-01 20:42:26 +0300

    Fix #24 | Improve card details

--- src/pages/CardsDetailsPage.jsx
+++ src/pages/CardsDetailsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
-import { Box, Container, Divider, Link, Paper, Typography } from '@mui/material';
+import { Box, Container, Divider, Grid, Link, Paper, Typography } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { pink } from '@mui/material/colors';
 import PageHeader from '../components/PageHeader';
@@ -8,6 +8,10 @@ import Spinner from '../components/Spinner';
 import Error from '../components/Error';
 import useCards from '../hooks/useCards';
 import { useTheme } from '../providers/CustomThemeProvider';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import PlaceIcon from '@mui/icons-material/Place';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 const favoriteColor = pink[500];
 
@@ -24,29 +28,32 @@ export default function CardsDetailsPage() {
   if (error) return <Error errorMessage={error} />;
   console.log(card);
   return (
-    <>
-      <PageHeader title="Previewing card" subtitle={id} />
-      <Paper sx={{ backgroundColor: isDark ? '#000' : '#EEE', display: 'flex', gap: 3, mx: 7, borderRadius: 5 }}>
+    <Grid sx={{ pt: 15 }}>
+      <Paper sx={{ backgroundColor: isDark ? '#171717' : '#FFF', display: 'flex', gap: 3, mx: 7, borderRadius: 5, maxHeight: '600px' }}>
         <Box component='img' src={card.image.url} alt={card.image.alt} sx={{ width: '50%', borderRadius: 5 }} />
         <Box flexGrow={1} sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Box flexGrow={2}>
-            <Typography variant='h3'>{card.title}</Typography>
+          <Box flexGrow={2} sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <Typography variant='h3' sx={{ pt: 3 }}>{card.title}</Typography>
             <Typography variant='subtitle1' gutterBottom sx={{ fontWeight: 'bold' }}>{card.subtitle}</Typography>
             <Divider variant='middle' />
+            <Typography gutterBottom sx={{ fontWeight: 'bold' }}>Business Description:</Typography>
             <Typography gutterBottom>{card.description}</Typography>
-            <Typography gutterBottom>Email: {card.email}</Typography>
-            <Typography gutterBottom>Address: {card.address.street} {card.address.houseNumber}, {card.address.city}, {card.address.country}</Typography>
-            <Typography>Website: <Link href="#">{card.web}</Link></Typography>
+            <Divider variant='middle' />
+            <Typography gutterBottom sx={{ display: 'flex', alignItems: 'center' }} ><EmailIcon /> &nbsp; {card.email}</Typography>
+            <Typography gutterBottom sx={{ display: 'flex', alignItems: 'center' }}><PhoneIcon /> &nbsp; {card.phone}</Typography>
+
+            <Typography gutterBottom sx={{ display: 'flex', alignItems: 'center' }}><PlaceIcon /> &nbsp; {card.address.street} {card.address.houseNumber}, {card.address.city}, {card.address.country}</Typography>
+            <Typography sx={{ display: 'flex', alignItems: 'center' }}><OpenInNewIcon /> &nbsp; <Link href="#">{card.web}</Link></Typography>
           </Box>
           <Box>
             <Divider variant='middle' />
             <Container sx={{ display: 'flex', justifyContent: 'center' }}>
-              <Typography sx={{ color: favoriteColor, fontWeight: 'bold' }}>{card.likes.length}&nbsp;</Typography>
+              <Typography sx={{ color: favoriteColor, fontWeight: 'bold' }}>This business has been liked by {card.likes.length} users&nbsp;</Typography>
               <FavoriteIcon sx={{ color: favoriteColor }} />
             </Container>
           </Box>
         </Box>
       </Paper>
-    </>
+    </Grid>
   )
 }
