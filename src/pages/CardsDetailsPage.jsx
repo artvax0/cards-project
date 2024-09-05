90f5c82b8c79465c60f64698adac285db813fc82
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-09-05 18:48:24 +0300

    Add Responsiveness

--- src/pages/CardsDetailsPage.jsx
+++ src/pages/CardsDetailsPage.jsx
@@ -28,10 +28,10 @@ export default function CardsDetailsPage() {
   if (error) return <Error errorMessage={error} />;
   console.log(card);
   return (
-    <Grid sx={{ pt: 15 }}>
-      <Paper sx={{ backgroundColor: isDark ? '#171717' : '#FFF', display: 'flex', gap: 3, mx: 7, borderRadius: 5, maxHeight: '600px' }}>
-        <Box component='img' src={card.image.url} alt={card.image.alt} sx={{ width: '50%', borderRadius: 5 }} />
-        <Box flexGrow={1} sx={{ display: 'flex', flexDirection: 'column' }}>
+    <Box flexGrow='1' display='flex' alignItems='center' py={3}>
+      <Paper sx={{ backgroundColor: isDark ? '#171717' : '#FFF', display: 'flex', gap: 3, mx: 7, borderRadius: 5, maxHeight: { xs: 'auto', md: '600px' }, flexDirection: { xs: 'column', md: 'row' } }}>
+        <Box component='img' src={card.image.url} alt={card.image.alt} sx={{ width: { xs: '100%', md: '50%' }, borderRadius: 5 }} />
+        <Box flexGrow={1} sx={{ display: 'flex', flexDirection: 'column' }} p={2}>
           <Box flexGrow={2} sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
             <Typography variant='h3' sx={{ pt: 3 }}>{card.title}</Typography>
             <Typography variant='subtitle1' gutterBottom sx={{ fontWeight: 'bold' }}>{card.subtitle}</Typography>
@@ -48,12 +48,12 @@ export default function CardsDetailsPage() {
           <Box>
             <Divider variant='middle' />
             <Container sx={{ display: 'flex', justifyContent: 'center' }}>
-              <Typography sx={{ color: favoriteColor, fontWeight: 'bold' }}>This business has been liked by {card.likes.length} users&nbsp;</Typography>
+              <Typography textAlign='center' sx={{ color: favoriteColor, fontWeight: 'bold' }}>This business has been liked by {card.likes.length} users&nbsp;</Typography>
               <FavoriteIcon sx={{ color: favoriteColor }} />
             </Container>
           </Box>
         </Box>
       </Paper>
-    </Grid>
+    </Box>
   )
 }
