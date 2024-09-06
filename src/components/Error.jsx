14b3eae0df6fa4c27c08c532f4e15c9380a2692a
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-09-06 23:29:18 +0300

    Improve Error Response

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -4,22 +4,22 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 const Error = ({ errorMessage }) => {
     return (
-    <Container>
-        <Grid container spacing={2}>
-        <Grid item xs={12} md={8}>
-            <Typography variant="h5" color="initial">
-            Oops... something went wrong: {errorMessage}
-            </Typography>
-        </Grid>
-        <Grid item xs={12} md={4} justifyContent="center">
-            <img
-            width="100%"
-            src="public/error404.png"
-            alt="broken robot"
-            />
-        </Grid>
-        </Grid>
-    </Container>
+        <Container>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={8}>
+                    <Typography variant="h5" color="initial">
+                        Oops... Something went wrong, contact a system administrator, error message: <strong>{errorMessage}</strong>
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={4} justifyContent="center">
+                    <img
+                        width="100%"
+                        src="/paper-shredder.png"
+                        alt="broken robot"
+                    />
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 export default Error;
