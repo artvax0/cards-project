ce1389169725a320924993c8bff667faed2cb141
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-09-07 01:15:49 +0300

    Fix #29 | Hide searchbar on unnecessary pages

--- src/pages/layout/header/right-navigation/SearchBar.jsx
+++ src/pages/layout/header/right-navigation/SearchBar.jsx
@@ -1,7 +1,7 @@
 import { Box, FormControl, InputAdornment, OutlinedInput } from '@mui/material'
-import React, { memo, useCallback } from 'react'
+import React, { memo, useCallback, useEffect } from 'react'
 import { useTheme } from '../../../../providers/CustomThemeProvider'
-import { useSearchParams } from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 import { debounce } from '../../../../utils/debounce';
 
@@ -9,22 +9,24 @@ export default memo(function SearchBar() {
   const { isDark } = useTheme();
   const [searchParams, setSearch] = useSearchParams();
   const handleChange = useCallback(debounce(({ target }) => setSearch({ q: target.value }), 250), [setSearch]);
+  const location = useLocation();
 
-  return (
-    <Box>
-      <FormControl>
-        <OutlinedInput
-          size='small'
-          placeholder='Search'
-          defaultValue={searchParams.get('q') ?? ''}
-          onChange={handleChange}
-          startAdornment={
-            <InputAdornment
-              position='start'><SearchIcon
-                aria-label='search' />
-            </InputAdornment>}
-          sx={{ backgroundColor: isDark ? '#222831' : '#EEEEEE' }} />
-      </FormControl>
-    </Box>
-  )
+  if (location.pathname == '/' || location.pathname == '/cards')
+    return (
+      <Box>
+        <FormControl>
+          <OutlinedInput
+            size='small'
+            placeholder='Search'
+            defaultValue={searchParams.get('q') ?? ''}
+            onChange={handleChange}
+            startAdornment={
+              <InputAdornment
+                position='start'><SearchIcon
+                  aria-label='search' />
+              </InputAdornment>}
+            sx={{ backgroundColor: isDark ? '#222831' : '#EEEEEE' }} />
+        </FormControl>
+      </Box>
+    )
 })
