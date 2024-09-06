8bc3d47fa095ec54c5968d29bdf62f231191cd3b
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-09-06 23:28:50 +0300

    Fix #28 | Enhance Error Page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,41 +1,46 @@
 import React from 'react'
-import PageHeader from '../components/PageHeader'
-import { Box, Button, Divider, Paper, Typography } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
+import { Box, Container, Divider, Grid, Paper, Typography } from '@mui/material'
 import { ROUTES } from '../routes/routesModel';
 import { useTheme } from '../providers/CustomThemeProvider';
+import NavBarItem from '../components/routes/NavBarItem';
 
 export default function ErrorPage() {
   const { isDark } = useTheme();
-  const navigate = useNavigate();
   return (
     <>
-      <Box display='flex' justifyContent='center' alignItems='center' px={20} minHeight='inherit'>
-        <Paper elevation={10} sx={{ backgroundColor: isDark ? '#222831' : '#EEEEEE', flexGrow: '1', display: 'flex', p: 4, borderRadius: 5, gap: 5 }}>
-          <Box className='icon' flex={1} sx={{ aspectRatio: '1/1', backgroundColor: '#fff', borderRadius: 5, backgroundImage: 'url(public/not-found.svg)', backgroundRepeat: 'no-repeat', backgroundSize: 'contain', backgroundPosition: 'center' }} />
-          <Box className='info' flex={2}>
-            <Typography variant='h3' gutterBottom fontWeight='bold'>Oops! We ran out of business cards.</Typography>
-            <Divider variant='middle' />
-            <Typography variant='h4' mt={2}>The page you were for does not exist!</Typography>
-            <Typography variant='h4' mt={2}>You may have entered the wrong page address, or the page has moved.</Typography>
-            <Box display='flex' justifyContent='center'>
-              <Button onClick={navigate(ROUTES.ROOT)}>Return Home</Button>
-            </Box>
-
-          </Box>
+      <Container maxWidth='md' sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
+        <Paper elevation={10} sx={{ backgroundColor: isDark ? '#222831' : '#EEEEEE', p: 3 }}>
+          <Grid container alignItems='stretch'>
+            <Grid item xs={12} md={4} sx={{ backgroundColor: '#fff', aspectRatio: '1/1', display: 'flex', alignItems: 'center', justifyContent: 'center' }} borderRadius={5}>
+              <img
+                src='/not-found.svg'
+                alt='Not Found'
+                style={{
+                  width: '100%',
+                  height: 'auto',
+                  objectFit: 'cover'
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} md={8} pl={2}>
+              <Typography fontWeight='bold' variant='h5' component='h1' gutterBottom>
+                Oops! We ran out of business cards.
+              </Typography>
+              <Divider variant='middle' />
+              <Typography variant='body1' paragraph>
+                The page you were for does not exist! <br />
+                You may have entered the wrong page address, or the page has moved.
+              </Typography>
+              <Box display='flex' justifyContent='center' alignItems='center'>
+                <NavBarItem
+                  to={ROUTES.ROOT}
+                  label='return home'
+                />
+              </Box>
+            </Grid>
+          </Grid>
         </Paper>
-      </Box >
-      {/* <Box display='flex' justifyContent='space-between'>
-        <Box flexGrow='1'>
-          <Button onClick={() => navigate(ROUTES.ROOT)}>Return to Home Page</Button>
-        </Box>
-        <Box
-          component='img'
-          src='public/error404.png'
-          alt='Error 404, Page Not Found'
-        >
-        </Box>
-      </Box> */}
+      </Container>
     </>
   )
 }
