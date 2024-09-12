89b991f8d0ca3a57e4ccc8d26146a00bd6d7fe65
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-09-12 18:28:56 +0300

    Fix error message theme

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { useTheme } from "../providers/CustomThemeProvider";
 const Error = ({ errorMessage }) => {
+    const { isDark } = useTheme();
     return (
         <Container>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={8}>
-                    <Typography variant="h5" color="initial">
+                    <Typography variant="h5" color={isDark ? '#EEEEEE' : '#222831'}>
                         Oops... Something went wrong, contact a system administrator, error message: <strong>{errorMessage}</strong>
                     </Typography>
                 </Grid>
